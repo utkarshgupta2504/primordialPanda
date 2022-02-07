2c4f54c806f6ae34e238dca0c40b61e2846f4c38
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-02-08 04:51:08 +0530

    Reminder functionality done

--- cogs/timedTasks.py
+++ cogs/timedTasks.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from mimetypes import init
+import time
 from discord.ext import commands, tasks
 from discord.utils import get
 import discord
 import datetime
+import pytimeparse
 import json
 import os
 import io
@@ -16,8 +19,166 @@ class TimedTasks(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.isInitialised = False
+        self.reminders = {}
         self.timer.start()
         self.backupTimer.start()
+        self.reminderLoop.start()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        with open("database/reminders.json", "r") as f:
+            self.reminders = json.load(f)
+
+            self.isInitialised = True
+
+    @commands.group(name="reminder", case_insensitive=True)
+    async def reminder(self, ctx):
+        pass
+
+    @reminder.command(name="add")
+    async def reminderAdd(self, ctx: commands.Context, duration: str, *reminderString):
+
+        user = ctx.author.id
+        rem = " ".join(reminderString)
+
+        parsedTime = pytimeparse.parse(duration)
+
+        if not parsedTime:
+            await ctx.reply("Invalid time format, use `*w*d*h*m*s`")
+            return
+
+        parsedTime = str(int(time.time()) + parsedTime)
+
+        if parsedTime not in self.reminders:
+            self.reminders[parsedTime] = []
+
+        self.reminders[parsedTime].append(
+            {"reminder": rem, "user": user, "repeat": None, "channel": str(ctx.channel.id)})
+
+        with open("database/reminders.json", "w") as f:
+
+            json.dump(self.reminders, f, indent=2)
+
+        await ctx.reply("Reminder added!")
+
+    @reminder.command(name="repeat", aliases=["rep", "recurring", "recur"])
+    async def repeatingReminder(self, ctx, initialDuration, repeatingDuration, *reminderString):
+
+        user = ctx.author.id
+        rem = " ".join(reminderString)
+
+        parsedTime = pytimeparse.parse(initialDuration)
+
+        if not parsedTime:
+            await ctx.reply("Invalid initial time format, use `*w*d*h*m*s`")
+            return
+
+        parsedRepeatingTime = pytimeparse.parse(repeatingDuration)
+
+        if not parsedRepeatingTime:
+            await ctx.reply("Invalid repeating time format, use `*w*d*h*m*s`")
+            return
+
+        parsedTime = str(int(time.time()) + parsedTime)
+
+        if parsedTime not in self.reminders:
+            self.reminders[parsedTime] = []
+
+        self.reminders[parsedTime].append(
+            {"reminder": rem, "user": user, "repeat": parsedRepeatingTime, "channel": str(ctx.channel.id)})
+
+        with open("database/reminders.json", "w") as f:
+
+            json.dump(self.reminders, f, indent=2)
+
+        await ctx.reply("Repeating Reminder added!")
+
+    @reminder.command(name="delete", aliases=["del", "remove", "rem"])
+    async def deleteTimer(self, ctx, *deleteReminder):
+
+        found = False
+
+        for rem in self.reminders:
+
+            for pos, reminder_data in enumerate(self.reminders[rem]):
+
+                if reminder_data["user"] == ctx.author.id and " ".join(deleteReminder) in reminder_data["reminder"]:
+
+                    found = True
+                    self.reminders[rem].pop(pos)
+                    break
+
+            else:
+
+                continue
+
+            break
+
+        with open("database/reminders.json", "w") as f:
+
+            json.dump(self.reminders, f, indent=2)
+
+        await ctx.reply("Reminder removed!" if found else "No such reminder!")
+
+    @reminder.command(name="list", aliases=["show"])
+    async def reminderList(self, ctx):
+
+        id = ctx.author.id
+
+        reminders = []
+
+        for rem in self.reminders:
+            for reminder_data in self.reminders[rem]:
+
+                if(reminder_data["user"] == id):
+                    reminders.append(reminder_data)
+
+        if len(reminders) > 0:
+
+            await ctx.send("Your reminders\n\n>>> " + "\n".join(map(str, reminders)))
+
+        else:
+
+            await ctx.reply("You have no pending reminders")
+
+    @tasks.loop(seconds=5)
+    async def reminderLoop(self):
+        if not self.isInitialised:
+            return
+
+        currTime = int(time.time())
+        remindersToAdd = {}
+
+        for key in self.reminders:
+
+            if int(key) < currTime:
+
+                for reminderData in self.reminders[key]:
+                    await self.bot.get_channel(int(reminderData["channel"])).send("<@!" + str(reminderData["user"]) + ">" + " Reminder: " + reminderData["reminder"])
+
+                    if reminderData["repeat"]:
+                        updatedTime = str(
+                            int(time.time()) + reminderData["repeat"])
+
+                        if updatedTime not in remindersToAdd:
+                            remindersToAdd[updatedTime] = []
+
+                        remindersToAdd[updatedTime].append(reminderData)
+
+                self.reminders[key] = []
+
+        self.reminders.update(remindersToAdd)
+
+        self.reminders = {k: v for (k, v) in self.reminders.items() if v}
+
+        with open("database/reminders.json", "w") as f:
+
+            json.dump(self.reminders, f, indent=2)
+
+    @reminderLoop.before_loop
+    async def beforeReminder(self):
+        await self.bot.wait_until_ready()
 
     @tasks.loop(minutes=5)
     async def timer(self):
